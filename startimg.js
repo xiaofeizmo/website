0c5ded39c39a1bee3efdd118112971b4dfa98bec
xiaofeizmo <xiaofei.z.mo@gmail.com>
2023-03-30 22:01:30 -0400

    updated to VH_0330_10P

--- startimg.js
+++ startimg.js
@@ -1,31 +1,22 @@
 // Array of image URLs
-const images = [
-  "./images/collaborations1.jpg",
-  "./images/collaborations2.jpg",
-  "./images/collaborations3.jpg",
-  "./images/collaborations4.jpg",
-  "./images/collaborations5.jpg",
-  "./images/collaborations7.jpg",
-  "./images/collaborations8.jpg",
-  "./images/memories1.jpg",
-  "./images/memories2.jpg",
-  "./images/memories3.jpg",
-  "./images/memories4.jpg",
-  "./images/memories5.jpg",
-  "./images/memories6.jpg",
-  "./images/memories7.jpg",
-  "./images/work1.jpg",
-  "./images/work2.jpg",
-  "./images/work3.jpg",
-  "./images/work4.jpg",
-  "./images/work5.jpg",
-  "./images/work6.jpg",
-  "./images/work7.jpg",
+const startcode = [
+  '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/328106931?h=df95fc8e93&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed" class="video" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+  '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/481448579?h=93b0e6d624&amp&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+  '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/799674183?h=f69d90bdd2&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed" class="video" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+  '<div style="padding:56.25% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/812990780?h=32d5b2b0d9&title=0&byline=0&portrait=0&speed=0&badge=0&autopause=0&player_id=0&app_id=58479/embed" class="video" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen frameborder="0" style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+  '<img src="./images/work4.jpg">',
+  '<img src="./images/work5.jpg">',
+  '<img src="./images/collaborations4.jpg">',
+  '<img src="./images/memories2.jpg">',
+  '<video autoplay loop muted playsinline style="width:100%;"><source src="./images/memories3.mp4" type="video/mp4" /></video>',
+  '<img src="./images/memories5.jpg">',
 ];
 
-// Get a random image URL from the array
-const randomImage = images[Math.floor(Math.random() * images.length)];
+  // Get a random index
+  random_index = Math.floor(Math.random() * startcode.length);
 
-// Populate the div with the random image
-const startimg = document.getElementById("wrapper");
-startimg.innerHTML = `<img src="${randomImage}" alt="Random Image">`;
+  // Get code using the random_index
+  selected_code = startcode[random_index]
+
+  // Insert the code
+  document.getElementById('wrapper').innerHTML = `${selected_code}`
